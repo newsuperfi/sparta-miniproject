6fefc428253e3b063cecf3d1039e5f0675103829
newsuperfi <122774009+newsuperfi@users.noreply.github.com>
2023-05-17 21:17:49 +0900

    API 경로 수정
    튜터님 조언대로 좀 더 RESTful하게 API의 경로를 수정하였습니다.

--- app.py
+++ app.py
@@ -58,6 +58,26 @@ def membertable_post():
     return jsonify({'msg': '저장 완료!'})
 
 
+@app.route("/members/<memberId>", methods=["PUT"])
+def membertable_edit(memberId):
+    member_receive = request.form['member_give']
+    mbti_receive = request.form['mbti_give']
+    motive_receive = request.form['motive_give']
+    blog_receive = request.form['blog_give']
+    github_receive = request.form['github_give'] 
+
+    db.member.update_one({'memberId':memberId},
+    {'$set':{
+        'member': member_receive,
+        'mbti':mbti_receive,
+        'motive' : motive_receive,
+        'blog': blog_receive,
+        'github': github_receive
+        }})
+
+    return jsonify({'msg': '수정 완료!'})
+
+
 # 팀원 소개 페이지 삭제 버튼 클릭 -> DELETE /members/memberId 요청 (memberId부분은 팀원에 따라 변동되므로 동적URL ex) 원희: /members/wonhee 승준: /members/seungjun)
 # -> member 테이블 내에서 'memberId' 필드가 전송받은 memberId인 데이터를 한개 찾아서 삭제
 # 플라스크에서는 동적URL을 처리할 때 받아온 파라미터 부분을 꺽새로 감싸주고( <memberId> ), 함수 내부에서는 일반 변수형태로 사용
@@ -71,7 +91,7 @@ def membertable_delete(memberId):
 # 팀원 소개 페이지 방명록 등록 버튼 클릭 -> POST /guestbook/memberId 요청 
 # -> 전송받은 폼데이터를 guestbook 테이블에 저장 
 # -> json형태의 저장완료 메시지를 클라이언트로 전송
-@app.route("/guestbook/<memberId>", methods=["POST"])
+@app.route("/members/<memberId>/guestbook", methods=["POST"])
 def guestbook_post(memberId):
     nick_receive = request.form['nick_give']
     comment_receive = request.form['comment_give']
@@ -86,9 +106,12 @@ def guestbook_post(memberId):
 
     return jsonify({'msg': '저장 완료!'})
 
+
+    db.users.update_one({'name':'bobby'},{'$set':{'age':19}})
+
 # 팀원 소개 페이지 로드 시 show_comment() 함수 자동 실행 -> GET /guestbook/memberId 요청 
 # -> guestbook 테이블 내에 있는 데이터 중 동적URL로 전송받은 memberId에 해당되는 데이터를 찾아 json 형태로 클라이언트로 전송
-@app.route("/guestbook/<memberId>", methods=["GET"])
+@app.route("/members/<memberId>/guestbook", methods=["GET"])
 def guestbook_get(memberId):
     all_comments = list(db.guestbook.find({'memberId': memberId},{'_id':False}))
 
@@ -105,10 +128,9 @@ def member_get(memberId):
 
 # 팀원 소개 페이지 내에서 방명록 삭제 버튼 클릭 -> DELETE /guestbook/memberId 요청 
 # -> member 테이블 내에서 'memberId' 필드가 전송받은 memberId인 데이터를 한개 찾아서 삭제
-@app.route("/guestbook/<memberId>", methods=["DELETE"])
+@app.route("/members/<memberId>/guestbook", methods=["DELETE"])
 def guestbook_delete(memberId):
-    
-    db.guestbook.delete({'memberId': memberId})
+    db.guestbook.delete_one({'memberId': memberId})
     return jsonify({'msg':'삭제 완료!'})
 
 
